a1e229aa4e7013b222297061e58cdfd5a7130b20
Fnzlucas <fernandezlucas.pro@outlook.fr>
2026-03-24 21:26:40 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Alma.+', description: 'Trouvez vos clients automatiquement' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
