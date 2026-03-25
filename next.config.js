7eadb58c415944dbbda67d9b5f804354210d5b61
Fnzlucas <fernandezlucas.pro@outlook.fr>
2026-03-25 03:13:58 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/alma-plus-landing.html',
+        permanent: false,
+      },
+    ]
+  },
+}
 module.exports = nextConfig
